b3fd91f35d7dd72d318ce65a23f85bc51267c5ea
Bruno-Guerrisi <brunoguerrisi1973@outlook.it>
2021-11-23 15:46:47 +0100

    header

---
+++ src/components/header.vue
@@ -0,0 +1,95 @@
+<template>
+  <header>
+
+      <img src="@/assets/dc-logo.png" alt="dc logo">
+
+      <ul>
+          <li tabindex="0" v-for="(element, i) in linkNav" :key="`element-${i}`">
+              <a href="">{{element.text}}</a>
+            </li>
+      </ul>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data() {
+        return {
+            linkNav: [
+                {
+                    text: 'characters',
+                },
+                {
+                    text: 'comics',
+                },
+                {
+                    text: 'movies',
+                },
+                {
+                    text: 'tv',
+                },
+                {
+                    text: 'games',
+                },
+                {
+                    text: 'collectibles',
+                },
+                {
+                    text: 'videos',
+                },
+                {
+                    text: 'fans',
+                },
+                {
+                    text: 'news',
+                },
+                {
+                    text: 'shop',
+                },
+            ],
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+    header{
+        height: 120px;
+        padding: 10px 20px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        ul{
+            display: flex;
+            align-items: center;
+            list-style: none;
+            height: 100%;
+
+            li{
+                margin: 0 10px;
+                text-transform: uppercase;
+                height: 100%;
+                display: flex;
+                align-items: center;
+                border-bottom: 5px solid transparent;
+
+                &:focus,
+                &:hover{
+                    border-bottom-color: #0c7cec;
+                    color: #0c7cec;
+                    cursor: pointer;
+                }
+
+                a{
+                    text-decoration: none;
+                    color: currentColor;
+                    border: 0;
+                }
+            }
+
+        }
+    }
+</style>
